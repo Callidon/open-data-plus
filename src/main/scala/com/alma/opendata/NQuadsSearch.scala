0a07003d4422cf8de5cf1e7b78ad452f3516f903
Thomas Minier <tminier01@gmail.com>
2016-12-08 09:32:58 +0100

    use a TreeSet instead of a Set + rework the structure of the app to separate stage 1 and 2

--- src/main/scala/com/alma/opendata/NQuadsSearch.scala
+++ src/main/scala/com/alma/opendata/NQuadsSearch.scala
@@ -7,10 +7,17 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.eclipse.rdf4j.model.Statement
 import org.eclipse.rdf4j.rio.{RDFFormat, Rio}
 
+import scala.collection.immutable.TreeSet
+
 /**
-  * Created by thomas on 29/10/16.
+  * Web Data Commons Analyze built with Apache Spark
   */
 object NQuadsSearch {
+  /**
+    * Parse a NQuad in String format and get it's context
+    * @param line The string to parse
+    * @return
+    */
   def getContext(line : String) : String = {
     try {
       val reader = new StringReader(line)
@@ -21,23 +28,42 @@ object NQuadsSearch {
       case e: Exception => ""
     }
   }
+
+  /**
+    * Run stage one
+    * @param files
+    * @param sc
+    */
+  def runStageOne(files: String, sc : SparkContext) : Unit = {
+    val dataFile : RDD[String] = sc.textFile(files)
+    dataFile.filter(nquad => nquad.contains("Nantes") | nquad.contains("postal-code> \"44"))
+      .map(getContext)
+      .distinct()
+      .foreach(println)
+  }
+
+  /**
+    * Run stage two
+    * @param files
+    * @param stageOneFiles
+    * @param sc
+    */
+  def runStageTwo(files: String, stageOneFiles: String, sc : SparkContext) : Unit = {
+    val dataFile : RDD[String] = sc.textFile(files)
+    val stageOne  = sc.broadcast(TreeSet[String]() ++ sc.textFile(stageOneFiles).collect().toSet)
+
+    dataFile.filter(nquad => {
+      stageOne.value.contains(getContext(nquad))
+    }).foreach(println)
+  }
+
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf()
       .setAppName("NQuads Search")
     val sc = new SparkContext(conf)
 
-    val dataFile : RDD[String] = sc.textFile(args(0))
-
-    // collect all graphs which are related to a Nantes
-    val graphs = sc.broadcast(dataFile.filter(t => t.contains("Nantes") | t.contains("postal-code> \"44"))
-      .map(t => getContext(t))
-      .distinct()
-      .collect().toSet)
-
-    // find and print all triples containing one of the previous graphs
-    dataFile.filter(t => {
-      graphs.value.contains(getContext(t))
-    }).foreach(println)
+    //runStageOne(args(0), sc)
+    runStageTwo(args(0), args(1), sc)
   }
 
 }
